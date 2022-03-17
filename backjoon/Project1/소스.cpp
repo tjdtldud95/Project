2c8174365454879fc81fc7438a681803adf50527
성시영 <tjdtldud95@naver.com>
2022-03-17 21:47:58 +0900

    BackJoon
     17626

---
+++ backjoon/Project1/소스.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<stack>
+#include<algorithm>
+
+using namespace std;
+
+int num1 = 0, num0 = 0;
+int answer[1000];
+
+int fibonacci(int n)
+{
+    if (n == 0) {
+        num0++;
+        return 0;
+    }
+    else if (n == 1) {
+        num1++;
+        return 1;
+    }
+
+    if (answer[n] != 0)
+        return answer[n];
+
+    else
+    {
+        answer[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        return answer[n];
+    }
+}
+int main()
+{
+    int n, a;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        fibonacci(a);
+
+        cout << num0 << " " << num1;
+        num0 = 0;
+        num1 = 0;
+    }
+    return 0;
+}
+
+

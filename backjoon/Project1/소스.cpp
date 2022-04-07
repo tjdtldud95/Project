7090d464fa0e7c10c4bbe788f202d9fb82da60de
성시영 <tjdtldud95@naver.com>
2022-04-07 22:23:55 +0900

    BackJoon
    3052

--- backjoon/Project1/소스.cpp
+++ backjoon/Project1/소스.cpp
@@ -1,31 +1,27 @@
 ﻿#include<iostream>
-#include<vector>
 using namespace std;
-
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
-    int r; cin >> r;
+    int list[1000] = { 0 };
+    int tmp,answer = 0;
 
-    for (int i = 0; i < r; i++)
+    for (int i = 0; i < 10; i++)
     {
-        for (int j = r-1; j > i; j--)
-        {
-            cout << " ";
-        }
-
-        for (int j = 0; j <= i; j++)
-        {
-            cout << "*";
-        }
-
-        cout<<"\n";
+        cin >> tmp;
+        list[tmp % 42]++;
     }
 
+    for (auto it : list)
+    {
+        if (it != 0)
+            answer++;
+    }
+
+    cout << answer;
     return 0;
 }
 

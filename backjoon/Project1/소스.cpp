a9c4260bde3605d30e4c05f312b27f4fd533a0fa
성시영 <tjdtldud95@naver.com>
2022-04-26 23:21:24 +0900

    BackJoon
    2579

--- backjoon/Project1/소스.cpp
+++ backjoon/Project1/소스.cpp
@@ -1,28 +1,32 @@
 ﻿#include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
+
+int bp[100000];
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-
-	int* n = new int[5];
-	int answer = 0;
-
-	for (int i = 0; i < 5; i++)
-	{
-		cin >> n[i];
-	}
-
-	for (int i = 0; i < 5; i++)
-	{
-		answer += n[i] * n[i];
-	}
-
-	cout << answer%10;
-
 	
+	int n,tmp; cin >> n;
+	vector<int> list;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tmp;
+		list.push_back(tmp);
+	}
+
+	bp[0] = list[0];
+	bp[1] = list[0] + list[1];
+	bp[2] = max(list[0], list[1]) + list[2];
+	for (int i = 3; i < n; i++)
+	{
+		bp[i] = max(bp[i - 2] + list[i], bp[i - 3] + list[i - 1] + list[i] );
+	}
+
+	cout << bp[n - 1];
 	return 0;
 }

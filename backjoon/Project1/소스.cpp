f16b45488e7900af2f653171f6635361640c1d0a
성시영 <tjdtldud95@naver.com>
2022-05-17 22:53:31 +0900

    프로그래머스
    JadenCase 문자열 만들기

--- backjoon/Project1/소스.cpp
+++ backjoon/Project1/소스.cpp
@@ -7,106 +7,29 @@
 using namespace std;
 
 
-bool list[30][30];
-char bach[30][30];
 
-int solution(int m, int n, vector<string> board) {
-	int answer = 0;
-	int index = 0;
-	bool again;
-
-	for(auto its : board)
-	{
-		int len = its.size();
-		for (int i = 0; i < len; i++)
-		{
-			list[index][i] = true;
-			bach[index][i] = board[index][i];
-		}
-		index++;
-	}
-
-	do 
-	{
-		again = false;
-		for (int i = 0; i < m - 1; i++)
-		{
-			for (int j = 0; j < n - 1; j++)
-			{
-				if (bach[i][j] == '0' ) continue;
-
-				if (bach[i][j] == bach[i][j + 1]
-					&& bach[i + 1][j] == bach[i + 1][j + 1]
-					&& bach[i][j] == bach[i + 1][j + 1])
-				{
-					list[i][j] = false;
-					list[i][j + 1] = false;
-					list[i + 1][j] = false;
-					list[i + 1][j + 1] = false;
-
-					again = true;
-				}
-			}
-		}
-
-		for (int i = 0; i < m ; i++)
-		{
-			for (int j = 0; j < n ; j++)
-			{
-				if (list[i][j] == false)
-				{
-					bach[i][j] = '0';
-				}
-			}
-		}
-		
-		for (int i = m; i >0; i--)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (bach[i][j] == '0')
-				{
-					for (int pos = i - 1; ; pos--)
-					{
-						if (pos < 0)break;
-						if (bach[pos][j] != '0')
-						{
-							list[i][j] = true;
-							list[pos][j] = false;
-
-							bach[i][j] = bach[pos][j];
-							bach[pos][j] = '0';
-							break;
-						}
-					}
-				}
-			}
-			
-		}
-	} while (again);
+string a(string s)
+{
+	string answer = s;
+	int len = answer.size();
+	answer[0] = toupper(answer[0]);
 	
-	for (int i = 0; i < m; i++)
+	for (int i = 1; i < len; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			if (bach[i][j] == '0')
-			{
-				answer++;
-			}
-		}
+		answer[i - 1] == ' ' ? answer[i] = toupper(answer[i]) : answer[i] = tolower(answer[i]);
 	}
 
-
-	cout << answer;
 	return answer;
 }
+
+
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	solution(7, 2, { "AA", "BB", "AA", "BB", "ZZ", "ZZ", "CC" });
-
+	//solution(9, { 0,0,1,2,0,1,1,1,1,1,1 });
+	cout << a(" ");
 	return 0;
 }
